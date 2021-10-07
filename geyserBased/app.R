250720b0a6aab3d86c67241184dfdd5150798323
Statsgeek <statsgeeknz@googlemail.com>
2021-10-07 15:38:13 +0100

    Update app.R
    Add colour changes for the plotting

--- geyserBased/app.R
+++ geyserBased/app.R
@@ -1,6 +1,7 @@
 
 library(shiny)
 
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
@@ -14,7 +15,11 @@ ui <- fluidPage(
                         "Number of bins:",
                         min = 1,
                         max = 50,
-                        value = 30)
+                        value = 30), 
+            textInput(inputId = "HistColour", 
+                      label = "What colour for the plot?", 
+                      value = "blue"
+                        )
         ),
 
         # Show a plot of the generated distribution
@@ -33,7 +38,11 @@ server <- function(input, output) {
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        histCol <- ifelse(input$HistColour %in% colours(),
+                          input$HistColour, "blue")
+            
+            
+        hist(x, breaks = bins, col = histCol, border = 'white')
     })
 }
 
